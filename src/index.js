641c7a63278b3aa4504888029b68ea2b0d1819c6
BADERproductions <kevin.bader96@gmail.com>
2020-08-26 19:37:51 +0300

    ready for client demo

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import App from "./App";
 // Styles
 import "./index.scss";
 import "bootstrap/dist/css/bootstrap.css";
-import "react-phone-input-2/lib/style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
